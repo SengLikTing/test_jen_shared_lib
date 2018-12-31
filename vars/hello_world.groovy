1057f9eccb39822dbcc53030dd06ae2d9fa5df1b
Mudavn <46275679+SengLikTing@users.noreply.github.com>
2018-12-31 15:48:32 +0800

    Update hello_world.groovy

--- vars/hello_world.groovy
+++ vars/hello_world.groovy
@@ -4,5 +4,5 @@ def call(String name = 'human') {
   echo "Hello, ${name}."
   calMe "work?"
   int_nest_call "hahaha"
-  echo jen_lib.Environment.getAnimalCry(jen_lib.Environment.mouse)
+  echo Environment.getAnimalCry()
 }
