7c0f25e726a6087e90785af3132d41d51df6573b
Mudavn <46275679+SengLikTing@users.noreply.github.com>
2018-12-31 15:55:22 +0800

    Update bar.groovy

--- src/org/foo/bar.groovy
+++ src/org/foo/bar.groovy
@@ -1,1 +1,29 @@
-Environment
+#!/usr/bin/env groovy
+
+enum Environment {
+    elephant,
+    cat,
+    mouse,
+    tiger,
+    laohu
+
+    static getAnimalCry(Environment environment = Environment.mouse) {
+      switch(environment) {
+        case Environment.elephant: 
+          return "gao"
+          break
+        case Environment.cat:
+          return "meow"
+          break
+        case Environment.mouse:
+          return "zhizhi"
+          break
+        case Environment.tiger:
+          return "wao"
+          break
+        case Environment.laohu:
+          return "wao"
+          break
+      }
+    }
+}
