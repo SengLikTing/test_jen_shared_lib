743d85d00d00374dd3b41370c70918e997f79641
Mudavn <46275679+SengLikTing@users.noreply.github.com>
2018-12-31 17:24:03 +0800

    Update bar.groovy

--- src/org/foo/bar.groovy
+++ src/org/foo/bar.groovy
@@ -7,31 +7,28 @@ enum Environment {
     mouse,
     tiger,
     laohu
+
+    def getAnimalCry(Environment environment = Environment.mouse) {
+      switch(environment) {
+        case Environment.elephant: 
+          return "gao"
+          break
+        case Environment.cat:
+          return "meow"
+          break
+        case Environment.mouse:
+          return "zhizhi"
+          break
+        case Environment.tiger:
+          return "wao"
+          break
+        case Environment.laohu:
+          return "wao"
+          break
+        default:
+            return "mew"
+          break
+      }
+    }
 }
 
-def getAnimalCry(Environment environment = Environment.mouse) {
-  switch(environment) {
-    case Environment.elephant: 
-      return "gao"
-      break
-    case Environment.cat:
-      return "meow"
-      break
-    case Environment.mouse:
-      return "zhizhi"
-      break
-    case Environment.tiger:
-      return "wao"
-      break
-    case Environment.laohu:
-      return "wao"
-      break
-    default:
-        return "mew"
-      break
-  }
-}
-
-def jarja() {
-    return "har"   
-}
