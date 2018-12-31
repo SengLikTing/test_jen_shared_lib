bfa50fe5adb98e7f0d951e1187794b9317bc45d0
Mudavn <46275679+SengLikTing@users.noreply.github.com>
2018-12-31 16:58:10 +0800

    Update hello_world.groovy

--- vars/hello_world.groovy
+++ vars/hello_world.groovy
@@ -1,8 +1,32 @@
 #!/usr/bin/env groovy
 
+
+def getAnimalCry(Environment environment = Environment.mouse) {
+  switch(environment) {
+    case Environment.elephant: 
+      return "gao"
+      break
+    case Environment.cat:
+      return "meow"
+      break
+    case Environment.mouse:
+      return "zhizhi"
+      break
+    case Environment.tiger:
+      return "wao"
+      break
+    case Environment.laohu:
+      return "wao"
+      break
+    default:
+        return "mew"
+      break
+  }
+}
+
 def call(String name = 'human') {
   echo "Hello, ${name}."
   calMe "work?"
   int_nest_call "hahaha"
-  //echo bar.Environment.getAnimalCry()
+  echo getAnimalCry()
 }
