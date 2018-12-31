c616e8c316ee8a5a06e141f6b0dc1e4a5700f48b
Mudavn <46275679+SengLikTing@users.noreply.github.com>
2018-12-31 15:54:55 +0800

    Update int_nest_call.groovy

--- vars/int_nest_call.groovy
+++ vars/int_nest_call.groovy
@@ -1,32 +1,5 @@
 #!/usr/bin/env groovy
 
-enum Environment {
-    elephant,
-    cat,
-    mouse,
-    tiger,
-    laohu
-
-    static getAnimalCry(Environment environment = Environment.mouse) {
-      switch(environment) {
-        case Environment.elephant: 
-          return "gao"
-          break
-        case Environment.cat:
-          return "meow"
-          break
-        case Environment.mouse:
-          return "zhizhi"
-          break
-        case Environment.tiger:
-          return "wao"
-          break
-        case Environment.laohu:
-          return "wao"
-          break
-      }
-    }
-}
 def call(String nn = "int nesy") {
   echo "this is int nest call ${nn}"
   echo Environment.getAnimalCry(Environment.laohu)
