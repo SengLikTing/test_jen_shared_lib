6b16316c48acba2563a3a4850b34e432b75b3897
Mudavn <46275679+SengLikTing@users.noreply.github.com>
2018-12-31 18:22:38 +0800

    Update bar.groovy

--- src/org/foo/bar.groovy
+++ src/org/foo/bar.groovy
@@ -10,7 +10,7 @@ enum Elu {
 }
 
 class TestMe {
-    static def getAnimalCry(Elu environment = Elu.mouse) {
+    static def getAnimalCry() {
       /*  switch(environment) {
         case Elu.elephant: 
           return "gao"
