9703d212beeddc1446d93c0f1ff3941c5e15db32
Mudavn <46275679+SengLikTing@users.noreply.github.com>
2019-01-02 11:32:36 +0800

    Update bar.groovy

--- src/org/foo/bar.groovy
+++ src/org/foo/bar.groovy
@@ -1,14 +1,16 @@
 #!/usr/bin/env groovy
 package org.foo
 
-enum Elu {
-    elephant,
-    cat,
-    mouse,
-    tiger,
-    laohu
-}
+class bar {
+    static enum Elu {
+        elephant,
+        cat,
+        mouse,
+        tiger,
+        laohu
+    }
 
-def dummy() {
- return "niet"   
+    def dummy() {
+     return "niet"   
+    }
 }
