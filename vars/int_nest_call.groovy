3e282602379679fceddff5891f0b722d53016d35
Mudavn <46275679+SengLikTing@users.noreply.github.com>
2019-01-02 10:17:40 +0800

    Update int_nest_call.groovy

--- vars/int_nest_call.groovy
+++ vars/int_nest_call.groovy
@@ -4,3 +4,11 @@ def call(String nn = "int nesy") {
   echo "this is int nest call ${nn}"
   //echo new org.foo.bar().getAnimalCry(org.foo.Environment.cat)
 }
+
+def info(message) {
+    echo "INFO: ${message}"
+}
+
+def warning(message) {
+    echo "WARNING: ${message}"
+}
