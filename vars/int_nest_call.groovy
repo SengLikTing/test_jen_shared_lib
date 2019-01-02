5b8d43990a2b446101fb21938db0889e7bec1b8f
Mudavn <46275679+SengLikTing@users.noreply.github.com>
2019-01-02 10:20:18 +0800

    Update int_nest_call.groovy

--- vars/int_nest_call.groovy
+++ vars/int_nest_call.groovy
@@ -1,5 +1,12 @@
 #!/usr/bin/env groovy
 
+enum Aluminium {
+   kon,
+   rin,
+   zai,
+   dette
+}
+
 def call(String nn = "int nesy") {
   echo "this is int nest call ${nn}"
   //echo new org.foo.bar().getAnimalCry(org.foo.Environment.cat)
