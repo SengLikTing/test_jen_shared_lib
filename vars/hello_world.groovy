dbed6a5879bddcac176ab7e542e8e9be96e05c20
Mudavn <46275679+SengLikTing@users.noreply.github.com>
2019-01-02 11:30:25 +0800

    Update hello_world.groovy

--- vars/hello_world.groovy
+++ vars/hello_world.groovy
@@ -1,12 +1,12 @@
 #!/usr/bin/env groovy
-//import org.foo.*
+import static Em.*
  
 def call(String name = 'human') {
   echo "Hello, ${name}."
   calMe "work?"
   int_nest_call "hahaha"
   int_nest_call.info "gogo"
-  int_nest_call.warning(int_nest_call.Em.niet)
+  int_nest_call.warning(niet)
   //echo new Environment().getAnimalCry(Environment.tiger)
   //b = new TestMe()
   //echo getAnimalCry(Elu.cat)
