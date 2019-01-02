23bfabbc7c3727697f8c4822e6b7a4351d550964
Mudavn <46275679+SengLikTing@users.noreply.github.com>
2019-01-02 10:41:12 +0800

    Update int_nest_call.groovy

--- vars/int_nest_call.groovy
+++ vars/int_nest_call.groovy
@@ -1,5 +1,6 @@
 #!/usr/bin/env groovy
-
+import org.foo.*
+  
 def call(String nn = "int nesy") {
   echo "this is int nest call ${nn}"
   //echo new org.foo.bar().getAnimalCry(org.foo.Environment.cat)
@@ -9,9 +10,9 @@ def info(message) {
     echo "INFO: ${message}"
 }
 
-def warning(a) {
+def warning(bar a) {
    switch(a) {
-    case Elu.elephant:
+    case bar.elephant:
       echo "this is ele"
       break
    default:
